648a8479972463bb68d6fa998ae289056143b137
Karol Schweitzer <kschweitzer@ad.gence>
2018-08-30 14:52:44 +0200

    Game completed

---
+++ vue.js
@@ -0,0 +1,99 @@
+new Vue({
+    el: "#gameRoot",
+    data: {
+        gameStarted: false,
+        playerHealth: 100,
+        monsterHealth: 100,
+        turn: "player",
+        logs: []
+    },
+    methods: {
+        playGameToggle: function() {
+            this.gameStarted = !this.gameStarted
+            this.logs = [];
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.turn = "player"
+        },
+        attack: function(specialAttack) {
+            let maxValue = specialAttack ? 20 : 12;
+            let minValue = specialAttack ? 10 : 5;
+
+            let damage = ~~(Math.random() * (maxValue - minValue + 1) + minValue);
+
+
+            console.log(damage);
+
+            this.monsterHealth -= damage;
+
+            if (this.monsterHealth <= 0) {
+                this.endGame("monster");
+                addLog("player","kills", 0)
+                return;
+            }
+            this.addLog("player","attacks",damage);
+            this.setTurn();
+        },
+        monsterAttack: function() {
+            let damage = ~~(Math.random() * (12 - 5 + 1) + 5);
+
+            console.log(damage);
+            
+
+            this.playerHealth -= damage;
+            if (this.playerHealth <= 0) {
+                this.endGame("player");
+                addLog("monster","kills", 0);
+                return;
+            }
+            this.addLog("monster","attacks",damage);
+            this.setTurn();
+        },
+        endGame: function(lost) {
+            if (lost === "player") {
+                let message = "You lose, monster wins"
+            }
+            else {
+                let message = "You win, monster lost"
+            }
+            this.playGameToggle()
+        },
+        heal: function() {
+            let heal = ~~(Math.random() * (12 - 5 + 1) + 5);
+
+            this.playerHealth += heal;
+            if (this.playerHealth > 100) {
+                this.playerHealth = 100
+            }
+            this.addLog("player","heals",heal);
+            this.setTurn();
+        },
+        setTurn: function() {
+            if (this.turn === "player") {
+                this.turn = 'monster';
+            } 
+            else {
+                this.turn = 'player';
+            }
+        },
+        addLog: function(who,what,ammount) {
+            this.logs.unshift(
+                {
+                    side: who,
+                    action: what,
+                    number: ammount
+                }
+            )
+        }
+    },
+    computed: {
+
+    },
+    watch: {
+        turn: function() {
+            if (this.turn === 'monster') {
+                setTimeout(this.monsterAttack, 400);
+            }
+        }
+    }
+})
